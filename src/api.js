e0969aa07c9628a5361b249a9ebcb64fd06c99e5
ali khan <alikhxn0@gmail.com>
2024-04-23 18:23:38 -0400

    Squashed 'frontend/' changes from 90d18d7..4114673
    4114673 styling added
    866d00d fixed mainformID
    69f5116 added imports
    70f480a Added mainID to all forms
    8d816ad Merge pull request #9 from ilona-m88/year1
    c470964 Merge branch 'dev' into year1
    c944ec4 added logo on top, changed year 1 and made year 2 and year 3 (without extended months)
    4a29056 finished year1 form, added use Context to share isLoggedIn with all components
    f20fa61 Finished year 1 form
    594d860 Almost finished year1 logic
    1d7afa3 added more functionality to year1
    7322441 Finished year 1 form
    41a0fb0 Almost completed form
    7613657 Continued with other sections
    192e28b added functionality to year 1
    
    git-subtree-dir: frontend
    git-subtree-split: 4114673da0cfe3c294894e28d6b5f318ac56c5ce

--- src/api.js
+++ src/api.js
@@ -29,3 +29,51 @@ export const getUserData = (userId) => {
 export const updateUserProfile = (userId, userData) => {
   return api.put(`users/${userId}/`, userData);
 };
+
+//ALL form submits
+export const proFormaSubmit = (mainFormId, userData) => {
+  return api.post(`form/pro_forma/${mainFormId}`, userData);
+}
+export const revFormSubmit = (mainFormId, userData) => {
+  return api.post(`form/rev_form/${mainFormId}`, userData);
+}
+export const depreciationSubmit = (mainFormId, userData) => {
+  return api.post(`form/depreciation/${mainFormId}`, userData);
+}
+export const year1Submit = (mainFormId, yearNumber, userData) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`, userData);
+}
+export const year2Submit = (mainFormId, yearNumber, userData) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`, userData);
+}
+export const year3Submit = (mainFormId, yearNumber, userData) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`, userData);
+}
+
+//ALL form gets
+export const revFormGet = (mainFormId) => {
+  return api.get(`form/rev_form/${mainFormId}`);
+}
+export const proFormaGet = (mainFormId) => {
+  return api.get(`form/pro_forma/${mainFormId}`);
+}
+export const depreciationGet = (mainFormId) => {
+  return api.get(`form/depreciation/${mainFormId}`);
+}
+export const year1Get = (mainFormId, yearNumber) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`);
+}
+export const year2Get = (mainFormId, yearNumber) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`);
+}
+export const year3Get = (mainFormId, yearNumber) => {
+  return api.post(`/form/year_form/${mainFormId}/${yearNumber}`);
+}
+
+//Create and get main forms
+export const createMainForm = (userId, formName) => {
+  return api.post(`form/${userId}`, formName);
+}
+export const getMainForms = (userId) => {
+  return api.get(`form/${userId}`);
+}
